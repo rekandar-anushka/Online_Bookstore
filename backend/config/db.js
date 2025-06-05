5c73215c9e9fe43145d08a22fcc91695da0af0b0
anu-26 <anushka26092006@gmail.com>
2025-06-05 13:01:02 +0530

    Database config

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306, 
   user: process.env.DB_USER || 'your_mysql_user',
   password: process.env.DB_PASS || 'your_mysql_password',
   database: process.env.DB_NAME || 'online_bookstore',
